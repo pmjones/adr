ad84c3e54640d7ba28a2d147c924c77b48e97dab
Paul M. Jones <pmjones88@gmail.com>
2014-05-07 15:57:46 -0500

    streamline a bit

--- example-code/Blog/Responder/BlogAddResponder.php
+++ example-code/Blog/Responder/BlogAddResponder.php
@@ -5,12 +5,16 @@ class BlogReadResponder extends AbstractBlogResponder
 {
     public function __invoke()
     {
+        return $this->created()
+            || $this->responseView('add');
+    }
+
+    protected function created()
+    {
         if (isset($this->data->blog->id)) {
             $id = $this->data->blog->id;
             $this->response->redirect->created("/blog/read/{$id}");
             return $this->response;
         }
-
-        return $this->responseView('add');
     }
 }
