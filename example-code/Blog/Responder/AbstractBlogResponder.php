e7df9e51c3e7c681f651f66405949beb84cc24d2
Paul M. Jones <pmjones88@gmail.com>
2014-05-12 11:15:37 -0500

    set view names in abstract responder, not in browse responder

--- example-code/Blog/Responder/AbstractBlogResponder.php
+++ example-code/Blog/Responder/AbstractBlogResponder.php
@@ -5,6 +5,28 @@ use AbstractResponder;
 
 abstract class AbstractBlogResponder extends AbstractResponder
 {
+    protected function init()
+    {
+        $view_names = array(
+            'browse',
+            'read',
+            'edit',
+            'add',
+            'delete-failure',
+            'delete-success',
+            '_form',
+            '_intro',
+        );
+
+        $view_registry = $this->view->getViewRegistry();
+        foreach ($view_names as $view_name) {
+            $view_registry->set(
+                $view_name,
+                __DIR__ . "/views/{$view_name}.php"
+            );
+        }
+    }
+
     protected function notFound($key)
     {
         if (! $this->data->$key) {
