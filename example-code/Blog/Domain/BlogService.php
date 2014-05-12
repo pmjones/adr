0751349bd513cc6398b984391543f305fbf270c8
Paul M. Jones <pmjones88@gmail.com>
2014-05-12 11:19:58 -0500

    reduce body

--- example-code/Blog/Domain/BlogService.php
+++ example-code/Blog/Domain/BlogService.php
@@ -10,14 +10,19 @@ class BlogService
             array(
                 'id' => 1,
                 'author' => 'Paul M Jones',
-                'title' => 'The creator of aura',
-                'intro' => 'Paul M. Jones is an internationally recognized PHP expert who has worked as everything from junior developer to VP of Engineering in all kinds of organizations',
-                'body' => 'Paul M. Jones is an internationally recognized PHP expert who has worked as everything from junior developer to VP of Engineering in all kinds of organizations (corporate, military, non-profit, educational, medical, and others). Paul\'s latest open-source project is Aura for PHP. Among his other accomplishments, Paul is the lead developer of the Solar Framework, and the creator of the Savant template system. He has authored a series of authoritative benchmarks on dynamic framework performance, and was a founding contributor to the Zend Framework (the DB, DB_Table, and View components). Paul is a voting member of the PHP Framework Interoperability Group, where he shepherded the PSR-1 and PSR-2 recommendations, and was the primary author on the PSR-4 autoloader recommendation. He was also a member of the Zend PHP 5.3 Certification education advisory board. He blogs at paul-m-jones.com. In a previous career, Paul was an operations intelligence specialist for the US Air Force, and enjoys putting .308 holes in targets at 400 yards.'
+                'title' => 'About Paul',
+                'intro' => 'Paul M. Jones is an internationally recognized PHP expert ...',
+                'body' => <<<EOT 
+Paul M. Jones is an internationally recognized PHP expert who has worked as
+everything from junior developer to VP of Engineering in all kinds of
+organizations (corporate, military, non-profit, educational, medical, and
+others). Paul's latest open-source project is Aura for PHP.
+EOT;
             ),
             array(
                 'id' => 2,
                 'author' => 'Hari KT',
-                'title' => 'Something',
+                'title' => 'About Hari',
                 'intro' => 'Some introduction',
                 'body' => 'Some long content'
             )
