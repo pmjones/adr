2a57eaf32fd650efc4a7fbbef9f0db0f136d0620
Paul M. Jones <pmjones88@gmail.com>
2014-08-09 16:37:15 -0500

    move to top-level Domain namespace

---
+++ example-code/Domain/Blog/BlogEntity.php
@@ -0,0 +1,43 @@
+<?php
+namespace Domain\Blog;
+
+class BlogEntity
+{
+    public $id;
+    public $author;
+    public $title;
+    public $intro;
+    public $body;
+
+    protected $messages;
+
+    public function __construct($data = array())
+    {
+        $this->setData($data);
+    }
+
+    public function setMessages($messages)
+    {
+        $this->messages = $messages;
+    }
+
+    public function getMessages($messages)
+    {
+        return $this->messages;
+    }
+
+    public function setData($data = array())
+    {
+        foreach ($data as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->{$key} = $value;
+            }
+        }
+    }
+
+    public function getData()
+    {
+        $properties = get_object_vars($this);
+        return $properties;
+    }
+}
